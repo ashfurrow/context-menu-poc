2fedc7571a05038c517f163ac180b9997f108bde
Ash Furrow <ash@ashfurrow.com>
2026-04-18 17:38:05 -0300

    Initial Commit

---
+++ context-menu-poc/context_menu_pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  context_menu_pocApp.swift
+//  context-menu-poc
+//
+//  Created by Ash Furrow on 2026-04-18.
+//
+
+import SwiftUI
+
+@main
+struct context_menu_pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
